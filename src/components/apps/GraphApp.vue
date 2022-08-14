3074789df21d7365c1f242338e642c4cf0696aec
scenaristeur <scenaristeur@gmail.com>
2022-08-14 02:51:31 +0200

    add graph

--- src/components/apps/GraphApp.vue
+++ src/components/apps/GraphApp.vue
@@ -1,53 +1,20 @@
 <template>
   <div class="d-flex flex-column h-100">
-    <draggable
-      tag="ul"
-      class="list-group overflow-auto flex-grow-1"
-      animation="200"
-      group="tasks"
-      @change="draggableChange()"
-      v-model="tasks"
-    >
-      <li
-        v-for="(task, i) in tasks"
-        :key="i"
-        :id="`task-${i}`"
-        class="list-group-item d-flex flex-row align-items-center"
-        @click="toggleTaskState(task)"
-      >
-        <input
-          class="form-check-input me-1 me-2 flex-shrink-0"
-          type="checkbox"
-          :checked="task.done"
-        />
-        <span :class="{ 'task-done': task.done }">{{ task.name }}</span>
-      </li>
-      <div
-        v-if="tasks.length === 0"
-        class="
-          d-flex
-          justify-content-center
-          align-items-center
-          h-100
-          user-select-none
-        "
-      >
-        <span class="text-uppercase fs-08em" style="color: lightgray">
-          No tasks
-        </span>
-      </div>
-    </draggable>
+    <div   class="list-group overflow-auto flex-grow-1">
+      <div id="graph" ref="graph"></div>
+      {{nodes}}
+    </div>
     <div class="d-flex pt-3">
       <div class="input-group">
         <input
-          type="text"
-          class="form-control first-item-radius"
-          v-model="newTask"
-          maxlength="50"
-          placeholder="Enter task name..."
-          @keyup.enter="addTask()"
+        type="text"
+        class="form-control first-item-radius"
+        v-model="newNode"
+        maxlength="50"
+        placeholder="Enter node name..."
+        @keyup.enter="addNode()"
         />
-        <button class="btn btn-primary last-item-radius" @click="addTask()">
+        <button class="btn btn-primary last-item-radius" @click="addNode()">
           <i class="bi bi-plus"></i>
         </button>
       </div>
@@ -57,67 +24,96 @@
 
 <script>
 import * as _ from "lodash";
-import draggable from "vuedraggable";
+// import draggable from "vuedraggable";
+import ForceGraph3D from '3d-force-graph';
 
 export default {
   name: "GraphApp",
-  components: {
-    draggable,
-  },
+  // components: {
+  //   draggable,
+  // },
   props: {
     sync: Object,
   },
   data: () => ({
-    tasks: [],
-    newTask: "",
+    nodes: [],
+    links: [],
+    newNode: "",
   }),
   computed: {
-    checkedTasks() {
-      return _.some(this.tasks, { done: true });
+    checkedNodes() {
+      return _.some(this.nodes, { done: true });
     },
   },
   watch: {
-    checkedTasks(newVal) {
+    checkedNodes(newVal) {
       const iconBtn = newVal
-        ? [{ icon: "trash2", callback: this.clearDoneTasks }]
-        : [];
+      ? [{ icon: "trash2", callback: this.clearDoneNodes }]
+      : [];
       this.$emit("setHeaderButtons", iconBtn);
     },
   },
   created() {
-    this.tasks = this.sync.get("tasks") || [];
+    this.nodes = this.sync.get("nodes") || [];
 
     this.sync.observe((event) => {
-      if (event.keysChanged.has("tasks")) {
-        this.tasks = this.sync.get("tasks");
+      if (event.keysChanged.has("nodes")) {
+        this.nodes = this.sync.get("nodes");
       }
     });
+
+
+
+  },
+  mounted(){
+    // const N = 300;
+    // const gData = {
+    //   nodes: [...Array(N).keys()].map(i => ({ id: i })),
+    //   links: [...Array(N).keys()]
+    //   .filter(id => id)
+    //   .map(id => ({
+    //     source: id,
+    //     target: Math.round(Math.random() * (id-1))
+    //   }))
+    // };
+    let gData = {nodes: this.nodes, links: this.links}
+
+    this.graph= ForceGraph3D()(this.$refs.graph)
+    .graphData(gData)
+    .width(800)
+    .height(600)
   },
   methods: {
     draggableChange() {
-      this.sync.set("tasks", this.tasks);
+      this.sync.set("nodes", this.nodes);
     },
-    addTask() {
-      if (this.newTask !== "") {
-        this.tasks.push({ name: this.newTask, done: false });
-        this.sync.set("tasks", this.tasks);
-        this.newTask = "";
+    addNode() {
+      if (this.newNode !== "") {
+        this.nodes.push({ name: this.newNode, done: false });
+        let nodesClean = this.nodes.map(function(item) {
+          delete item.__threeObj;
+          return item;
+        });
+
+
+        this.sync.set("nodes", nodesClean);
+        this.newNode = "";
       }
     },
-    toggleTaskState(task) {
-      task.done = !task.done;
-      this.sync.set("tasks", this.tasks);
+    toggleNodeState(node) {
+      node.done = !node.done;
+      this.sync.set("nodes", this.nodes);
     },
-    clearDoneTasks() {
-      this.tasks = _.filter(this.tasks, (t) => !t.done);
-      this.sync.set("tasks", this.tasks);
+    clearDoneNodes() {
+      this.nodes = _.filter(this.nodes, (t) => !t.done);
+      this.sync.set("nodes", this.nodes);
     },
   },
 };
 </script>
 
 <style scoped>
-.task-done {
+.node-done {
   text-decoration: line-through;
   color: gray;
 }
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  YJS_WEBSOCKET: process.env.VUE_APP_YJS_WEBSOCKET || "ws://localhost:1234"
+  YJS_WEBSOCKET: process.env.VUE_APP_YJS_WEBSOCKET || "ws://192.168.1.107:1234"
 }
